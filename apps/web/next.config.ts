ff0dcc10a29c2ad5d15ce1d856c1b2679f4709b2
0xReese <102135854+0xreese@users.noreply.github.com>
2025-01-16 22:04:16 +0800

    feat: update shadcn ui

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -6,7 +6,9 @@ const config = {
   reactStrictMode: true,
 
   /** Enables hot reloading for local packages without a build step */
-  transpilePackages: [],
+  transpilePackages: [
+    '@mindworld/ui',
+  ],
 
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
