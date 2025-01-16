2dffba3d3473391e60d2ed3d2cc14edfe56187b3
0xReese <102135854+0xreese@users.noreply.github.com>
2025-01-16 16:21:13 +0800

    feat: update prettier config

--- apps/expo/eslint.config.mjs
+++ apps/expo/eslint.config.mjs
@@ -1,11 +1,11 @@
-import baseConfig from "@mindworld/eslint-config/base";
-import reactConfig from "@mindworld/eslint-config/react";
+import baseConfig from '@mindworld/eslint-config/base'
+import reactConfig from '@mindworld/eslint-config/react'
 
 /** @type {import('typescript-eslint').Config} */
 export default [
   {
-    ignores: [".expo/**", "expo-plugins/**"],
+    ignores: ['.expo/**', 'expo-plugins/**'],
   },
   ...baseConfig,
   ...reactConfig,
-];
+]
