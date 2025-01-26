304ed630a9c87db40d463e383bba5aabb2255376
0xReese <102135854+0xreese@users.noreply.github.com>
2025-01-26 11:39:05 +0800

    feat: update package db

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -3,8 +3,12 @@ import { drizzle } from 'drizzle-orm/vercel-postgres'
 
 import * as schema from './schema'
 
+export { schema }
+
 export const db = drizzle({
   client: sql,
   schema,
   casing: 'camelCase',
 })
+
+export type DB = typeof db
